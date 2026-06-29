0bd131f175e232a9a1787319814d24dd9611113d
Davey22 <rizqi.daffy32@smp.belajar.id>
2026-06-29 16:00:31 +0700

    cors

--- server/middleware/securityMiddleware.ts
+++ server/middleware/securityMiddleware.ts
@@ -14,6 +14,11 @@ const allowedOrigins: string[] = rawOrigins
     "http://localhost:8080",   // Vite dev server (current port)
     "http://127.0.0.1:5173",
     "http://127.0.0.1:8080",
+    "http://172.19.82.34:3000", // cors asem
+    "http://172.19.82.34:3001",
+    "http://172.19.82.34:8080",
+    "http://172.19.82.34:5173",
+    "http://172.19.82.34:4173",
   ];
 
 export const configuredCors = cors({
