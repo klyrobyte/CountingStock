45edd3fb1804e71eb66049146fe9904f8a09675f
Davey22 <rizqi.daffy32@smp.belajar.id>
2026-07-03 13:47:48 +0700

    tv section

--- server/middleware/securityMiddleware.ts
+++ server/middleware/securityMiddleware.ts
@@ -21,12 +21,20 @@ const allowedOrigins: string[] = rawOrigins
     "http://172.19.82.34:4173",
     //this section are for local development, you should add port 3000,3001,8080,5173,4173,...
     //
+<<<<<<< HEAD
     "http://192.168.31.152:5173",
     "http://192.168.31.152:3001",
     "http://192.168.31.152:3000",
     "http://192.168.31.152:4001",
     "http://192.168.31.152:8080"
     "http://counting-stock.sugity.co.id",
+=======
+    "http://192.168.95.152:5173",
+    "http://192.168.95.152:3001",
+    "http://192.168.95.152:3000",
+    "http://192.168.95.152:4001",
+    "http://192.168.95.152:8080"
+>>>>>>> b840b7e (tv section)
   ];
 
 export const configuredCors = cors({
